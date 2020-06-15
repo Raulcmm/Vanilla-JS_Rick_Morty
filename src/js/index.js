59f2bd79e9aac19afe415be6bb9d19d296edbbbb
Raúl <osvaldoracm@gmail.com>
2020-06-15 18:52:29 -0500

    buttons added to switch between characters

--- src/js/index.js
+++ src/js/index.js
@@ -2,21 +2,34 @@ import Api from './api.js';
 
 /**DOM */
 let card = document.querySelector('#card');
-
 /**instance of class api */
 let api = new Api();
-
+let ID_CHARACTER = -1;
 async function initApp() {
-	let entero = +(Math.random() * 20).toFixed(0);
-	if (entero > 0 && entero < 20) {
-		let dataApi = await api.getCharacters(entero);
-		console.log(dataApi);
-		renderCharacter(dataApi);
+	if (ID_CHARACTER == -1) {
+		ID_CHARACTER = +(Math.random() * 591).toFixed(0);
 	}
-	setTimeout(() => {
-		initApp();
-	}, 5000);
+	let dataApi = await api.getCharacters(ID_CHARACTER );
+	renderCharacter(dataApi);
 }
+
+document.addEventListener('click', (e) => {
+	let left = e.target.classList.contains('left');
+	let right = e.target.classList.contains('right');
+	if (left && ID_CHARACTER>1) {
+		ID_CHARACTER--;
+	} else if (right && ID_CHARACTER<591) {
+		ID_CHARACTER++;
+	}
+
+ initApp();
+	console.log(ID_CHARACTER);
+});
+function nextCharacter() {
+	console.log('hol');
+}
+
+function previousCharacter() {}
 
 function renderCharacter(data) {
 	let htmlContent = `
@@ -29,8 +42,12 @@ function renderCharacter(data) {
 		<p id="specie"> Specie: <span>${data.species}</span></p>
 		<p id="gender"> Gender: <span>${data.gender}</span></p>
 		<p id="location"> Location: <span>${data.location.name}</span></p>
+		<small id="id"> API Id: ${ID_CHARACTER}</small>
 	</div>
-
+	<div class="arrows-container">
+		<i class="fas fa-chevron-left arrow left" ></i>
+		<i class="fas fa-chevron-right arrow right"></i>
+	</div>
     `;
 	card.innerHTML = htmlContent;
 }
